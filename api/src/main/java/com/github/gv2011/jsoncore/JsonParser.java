043ac793872dbe5f8635c4ce6d71c6d8855fe61e
Vinz <gv2011@users.noreply.github.com>
2016-05-01 23:04:38 +0200

    Refactoring

---
+++ api/src/main/java/com/github/gv2011/jsoncore/JsonParser.java
@@ -0,0 +1,25 @@
+package com.github.gv2011.jsoncore;
+
+import java.io.Closeable;
+
+public interface JsonParser extends Closeable{
+
+  void beginObject();
+
+  JsonToken peek();
+
+  String nextName();
+
+  void endObject();
+
+  String nextString();
+
+  boolean nextBoolean();
+
+  void beginArray();
+
+  void endArray();
+
+  void nextNull();
+
+}
